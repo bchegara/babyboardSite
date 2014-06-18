4cc8638d132f808b2a310cdf582018442478018c
baptcheg <baptmanfr@gmail.com>
2014-06-19 01:47:31 +0200

    Modif des numéros utiles, bug sur l'ajout et problème d'affichage lorsqu'il n'y a pas de numéro utile associé à l'enfant

--- src/main/java/com/mycompany/babyboardsite/View/CategorieLayoutNumeroUtile.java
+++ src/main/java/com/mycompany/babyboardsite/View/CategorieLayoutNumeroUtile.java
@@ -10,21 +10,17 @@ package com.mycompany.babyboardsite.View;
  * @author baptman
  */
 import com.mycompany.babyboardsite.Data.Baby;
-import com.mycompany.babyboardsite.Data.MainFact;
 import com.mycompany.babyboardsite.MyVaadinUI;
 import static com.mycompany.babyboardsite.MyVaadinUI.navigator;
+import com.vaadin.data.util.sqlcontainer.RowId;
 import com.vaadin.data.util.sqlcontainer.SQLContainer;
 import com.vaadin.event.LayoutEvents;
 import com.vaadin.event.ShortcutAction;
-import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Table;
-import com.vaadin.ui.TextArea;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
@@ -36,6 +32,7 @@ public class CategorieLayoutNumeroUtile extends CategorieLayout {
 
     HorizontalLayout horizontalLayout;
     SQLContainer SQLContainerNumeroUtile;
+    Table table;
 
     public CategorieLayoutNumeroUtile(Baby baby) {
         super(baby);
@@ -104,15 +101,23 @@ public class CategorieLayoutNumeroUtile extends CategorieLayout {
         // Trivial logic for closing the sub-window
 //        int test = baby.numeroUtileCategorie.getListAdresse().size();
         SQLContainerNumeroUtile = baby.numeroUtileCategorie.getSQLContainer();
-        Table table = new Table("Parents", SQLContainerNumeroUtile);
+        table = new Table("Numéros Utiles", SQLContainerNumeroUtile);
         table.setPageLength(20); // the number of rows per page
         table.setSizeFull(); // the table will fill the window
         table.setImmediate(true); // the server is notify each time I select a row or modify values
         table.setSelectable(true); // the user is allowed to select rows
         table.setMultiSelect(false); // the user is not allowed to select more than one row
         table.setEditable(true); // the user is allowed to modify values in the selected row
+//a mettre dans le css de la table pour ne pas afficher l'élément permettant d'afficher les colonne caché
+//         .v-table-column-selector {
+//    height: 0px;
+//    width: 0px;
+//  } 
+        table.setColumnCollapsingAllowed(true);
+        table.setColumnCollapsed("idNumeroUtile", true);
+        table.setColumnCollapsed("idBaby", true);
         content.addComponent(table);
-        
+
         Button saveButton = new Button("Sauvegarder");
         saveButton.addClickListener(new Button.ClickListener() {
             public void buttonClick(Button.ClickEvent event) {
@@ -126,7 +131,64 @@ public class CategorieLayoutNumeroUtile extends CategorieLayout {
             }
         });
         content.addComponent(saveButton);
+
+        Button addContact = new Button("Ajouter un contact");
+        addContact.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+
+//               // On récupère la dernière ligne de la table parent
+//                Object lastentry = parent.lastItemId();
+//                // On cherche la valeur de la colonne idUser de cette derniere entrée
+//                Object lastidvalue = table.getContainerProperty(lastentry, "idUser").getValue();
+//                
+//                //on transforme cette valeur en int
+//                int idsup = Integer.parseInt(lastidvalue.toString())+1;
+//                //on retransforme cet int en objet pour l'afficher
+//                Object idnewparent = (int)idsup;
+//
+//                parent.removeAllContainerFilters();
+                Object contactId = table.addItem();
+                table.getContainerProperty(contactId, "idNumeroUtile").setValue(
+                        2);
+                table.getContainerProperty(contactId, "idBaby").setValue(
+                        2);
+                table.getContainerProperty(contactId, "idBaby").setValue(
+                        1);
+                table.getContainerProperty(contactId, "role").setValue(
+                        "role");
+                table.getContainerProperty(contactId, "nom").setValue(
+                        "nom");
+                //On incrémente automatiquement l'idUser
+                table.getContainerProperty(contactId, "numero").setValue(
+                        "+336");
+                table.getContainerProperty(contactId, "adresse").setValue(
+                        "aresse");
+
+                table.select(contactId);
+
+            }
+        });
+        content.addComponent(addContact);
         
+                Button deleteBt = new Button("Supprimer le contact");
+        deleteBt.addClickListener(new Button.ClickListener() {
+            public void buttonClick(Button.ClickEvent event) {
+                RowId itemID = (RowId) table.getValue(); // retrieves the id of the record
+                if (itemID != null) {
+                    SQLContainerNumeroUtile.removeItem(itemID); // remove the record from the container
+                }
+                try {
+                    SQLContainerNumeroUtile.commit();
+                } catch (UnsupportedOperationException ex) {
+                    Logger.getLogger(MyVaadinUI.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (SQLException ex) {
+                    Logger.getLogger(MyVaadinUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        );
+        content.addComponent(deleteBt);
+
         Button ok = new Button("OK+ " + SQLContainerNumeroUtile.size());
         ok.addClickListener(new Button.ClickListener() {
             public void buttonClick(Button.ClickEvent event) {
